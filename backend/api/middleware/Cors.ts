1abff596b4a531127aa9dadf45cbdfef1891ba5b
KellbisJ <kellbisdevsw@gmail.com>
2025-03-29 17:27:37 -0400

    adding new allowedOrigins

--- backend/api/middleware/Cors.ts
+++ backend/api/middleware/Cors.ts
@@ -2,10 +2,10 @@ import cors from 'cors';
 
 const allowedOrigins = [
 	'https://movies-ks-frontend.vercel.app',
+	'https://movies-ks-frontend-git-develop-kellbis-projects.vercel.app',
 	'https://movies-ks-backend.vercel.app',
 	'https://movies-ks-backend-git-master-kellbis-projects.vercel.app',
-	'https://movies-ks-backend-git-types-kellbis-projects.vercel.app',
-	'https://movies-ks-frontend-git-types-kellbis-projects.vercel.app',
+	'https://movies-ks-backend-git-develop-kellbis-projects.vercel.app',
 ];
 
 export const CorsOptions: cors.CorsOptions = {
