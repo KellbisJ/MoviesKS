7e63980eb57928adfd240c5104054accc599336d
KellbisJ <kellbisdevsw@gmail.com>
2025-04-10 21:33:25 -0400

    trying to solve issues

--- backend/api/middleware/Cors.ts
+++ backend/api/middleware/Cors.ts
@@ -1,18 +1,36 @@
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const allowedOrigins = [
 	'https://movies-ks-frontend.vercel.app',
 	'https://movies-ks-frontend-git-develop-kellbis-projects.vercel.app',
 	'https://movies-ks-backend.vercel.app',
-	'https://movies-ks-backend-git-master-kellbis-projects.vercel.app',
 	'https://movies-ks-backend-git-develop-kellbis-projects.vercel.app',
 ];
 
 export const CorsOptions: cors.CorsOptions = {
-	origin: allowedOrigins,
+	origin: (origin, callback) => {
+		console.log('origin:', origin);
+
+		if (!origin) return callback(null, true);
+
+		if (process.env.NODE_ENV === 'development') {
+			return callback(null, true);
+		}
+		if (process.env.NODE_ENV === 'test') {
+			return callback(null, true);
+		}
+
+		if (allowedOrigins.includes(origin)) {
+			return callback(null, true);
+		}
+
+		callback(new Error(`Origin '${origin}' not allowed by CORS`));
+	},
 	credentials: true,
 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 	allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
 };
-
 export const CorsMiddleware = cors(CorsOptions);
